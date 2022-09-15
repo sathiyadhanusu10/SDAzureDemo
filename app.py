35a94a076658b764a81f14a88b9d5ec5546d892e
sathiyadhanusu10 <94485870+sathiyadhanusu10@users.noreply.github.com>
2022-09-15 19:34:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "https://docs.microsoft.com/en-us/training/modules/migrate-app-service-migration-assistant/"
